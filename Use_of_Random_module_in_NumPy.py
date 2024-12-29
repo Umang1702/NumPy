364119ca9153c2b95789c9994f7c569d98433743
Umang Sharma <usharma1702@gmail.com>
2024-12-29 22:22:16 +0530

    Initial Commit

--- Use_of_Random_module_in_NumPy.py
+++ Use_of_Random_module_in_NumPy.py
@@ -5,26 +5,28 @@ import random # Import Random Module
 variable=np.random.rand(4)  # Use The rand() function of the Random module.
 print("Output of rand() function:\n",variable)
 print("\n")
+# The output of the rand() function gives the value or elementsbetween 0 and 1.
 
 variable1=np.random.rand(2,3)  # 2 -> Row and 3 -> Column
 print("Output of rand() function with column and rows values:\n",variable1)
 print("\n")
+# The output of the rand() function gives the value or elements betweem 0 and 1.
 
 # Use of randn() function of Random module:-
 variable2=np.random.randn(5)  # Use The randn() function of the Random module.
 print("Output of randn() function:\n",variable2)
 print("\n")
-# The output of the randn() function gives the value opr elements which is close to zero.
+# The output of the randn() function gives the value or elements which is close to zero.
 
 # Use of randf() function of Random module:-
-variable3=np.random.ranf(5)  # Use The randf() function of the Random module.
+variable3=np.random.ranf(5)  # Use The ranf() function of the Random module.
 print("Output of ranf() function:\n",variable3)
 print("\n")
 # The output of the randn() function gives the Floating value or Floating elements .
 
 
 # Use of randint() function of Random module:-
-variable4=np.random.randint(1,20,10)  # Use The randf() function of the Random module.
+variable4=np.random.randint(1,20,10)  # Use The randint() function of the Random module.
 print("Output of randint() function:\n",variable4)
 print("\n")
 # The output of the randint() function gives the integer value or element between the given range as a parameter.
