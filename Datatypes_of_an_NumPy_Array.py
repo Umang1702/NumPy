09291148dd2918974b5f9e72b2474e89bc3cd097
Umang Sharma <usharma1702@gmail.com>
2024-12-29 22:39:37 +0530

    Initial Commit

---
+++ Datatypes_of_an_NumPy_Array.py
@@ -0,0 +1,13 @@
+import numpy as np
+
+# For Integer Datatype:-
+var1=np.array([1,2,3,4,5])
+print("The Datatype of var1 is:",var1.dtype)
+
+# For Float Datatype:-
+var2=np.array([1.1,2.2,3.3,4.4,5.5])
+print("The Datatype of var2 is:",var2.dtype)
+
+# For String Datatype:-
+var3=np.array(["a","b","c","d"])
+print("The Datatype of var1 is:",var3.dtype)
