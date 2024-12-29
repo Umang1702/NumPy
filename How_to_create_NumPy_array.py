9ed0cdf68636eb7ca696870cf239863980eb334c
Umang Sharma <usharma1702@gmail.com>
2024-12-29 21:55:13 +0530

    Initial Commit

--- How_to_create_NumPy_array.py
+++ How_to_create_NumPy_array.py
@@ -17,3 +17,9 @@ arr3=np.array([[[1,2,3,4,5],[6,7,8,9,10],[11,12,13,14,15]]])
 print("Output of 3D array is: \n",arr3)
 print("Type of 3D array:",type(arr3))
 print("\n")
+
+
+# Higher-Dimensional Array:-
+arr10=np.array([1,2,3,4],ndmin=10)
+print("Output of 10D array is: \n",arr10)
+print("Type of 10D array:",type(arr10))
