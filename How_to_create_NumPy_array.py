b928b2f7a7dd4971cf3a93985aa1df22c3ae870d
Umang Sharma <usharma1702@gmail.com>
2024-12-29 21:37:58 +0530

    Initial Commit

---
+++ How_to_create_NumPy_array.py
@@ -0,0 +1,19 @@
+import numpy as np  # Importing the numpy library
+# 1-Dimensional Array:-
+arr1=np.array([1,2,3,4,5])
+print("Output of 1D array is: \n",arr1)
+print("Type 1D array:",type(arr1))
+print("\n")
+
+# 2-Dimensional Array:-
+arr2=np.array([[1,2,3,4,5],[6,7,8,9,10]])
+print("Output of 2D array is: \n",arr2)
+print("Type of 2D array:",type(arr2))
+print("\n")
+
+
+# 3-Dimensional Array:-
+arr3=np.array([[[1,2,3,4,5],[6,7,8,9,10],[11,12,13,14,15]]])
+print("Output of 3D array is: \n",arr3)
+print("Type of 3D array:",type(arr3))
+print("\n")
