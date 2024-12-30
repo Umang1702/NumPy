e1727ca9d9f3e928cd8e7b26167ff5f95bcf3d7a
Umang Sharma <usharma1702@gmail.com>
2024-12-30 19:32:29 +0530

    Initial Commit

---
+++ Arithmatical_Functions_in_NumPy_Array.py
@@ -0,0 +1,66 @@
+import numpy as np
+
+# Arithematical Functions for 1D Array:- 
+variable= np.array([11,12,13,14])
+print("The output of min() function is:",np.min(variable)) # Use of min() Function.
+print("\n")
+print("The output of max() function is:",np.max(variable))# Use of min() Function.
+print("\n")
+print("The output of sqrt() function is:",np.sqrt(variable))# Use of sqrt() Function.
+print("\n")
+print("The output of argmin() function is:",np.argmin(variable))# Use of argmin() Function.
+print("\n")
+print("The output of argmax() function is:",np.argmax(variable))# Use of argmax() Function.
+print("\n")
+print("The output of sin() function is:",np.sin(variable))# Use of sin() Function.
+print("\n")
+print("The output of cos() function is:",np.cos(variable))# Use of cos() Function.
+print("\n")
+print("The output of cumsum() function is:",np.cumsum(variable))# Use of cumsum() Function.
+print("\n")
+
+
+
+
+## Arithematical Functions for 2D Array:- 
+print("Arithematical Functions for 2 - Dimensional Arrays:-\n")
+variable_2= np.array([[100,50],[1,2]])
+print("The output of min() function is:",np.min(variable_2)) # Use of min() Function.
+print("\n")
+print("The output of max() function is:",np.max(variable_2))# Use of min() Function.
+print("\n")
+print("The output of sqrt() function is:",np.sqrt(variable_2))# Use of sqrt() Function.
+print("\n")
+print("The output of argmin() function is:",np.argmin(variable_2))# Use of argmin() Function.
+print("\n")
+print("The output of argmax() function is:",np.argmax(variable_2))# Use of argmax() Function.
+print("\n")
+print("The output of sin() function is:",np.sin(variable_2))# Use of sin() Function.
+print("\n")
+print("The output of cos() function is:",np.cos(variable_2))# Use of cos() Function.
+print("\n")
+print("The output of cumsum() function is:",np.cumsum(variable_2))# Use of cumsum() Function.
+print("\n")
+
+
+## Arithematical Functions for 3D Array:- 
+print("Arithematical Functions for 3 - Dimensional Arrays:-\n")
+variable_2= np.array([[[1,2,3],[4,5,6],[7,8,9]]])
+print("The output of min() function is:",np.min(variable_2)) # Use of min() Function.
+print("\n")
+print("The output of max() function is:",np.max(variable_2))# Use of min() Function.
+print("\n")
+print("The output of sqrt() function is:",np.sqrt(variable_2))# Use of sqrt() Function.
+print("\n")
+print("The output of argmin() function is:",np.argmin(variable_2))# Use of argmin() Function.
+print("\n")
+print("The output of argmax() function is:",np.argmax(variable_2))# Use of argmax() Function.
+print("\n")
+print("The output of sin() function is:",np.sin(variable_2))# Use of sin() Function.
+print("\n")
+print("The output of cos() function is:",np.cos(variable_2))# Use of cos() Function.
+print("\n")
+print("The output of cumsum() function is:",np.cumsum(variable_2))# Use of cumsum() Function.
+print("\n")
+
+
---
+++ Reciprocal_Function_in_NumPy_Array.py
@@ -0,0 +1,6 @@
+import numpy as np # Importing Numpy Libaray.
+
+# Use of reciprocal() function in NumPy:-
+variable = np.array([1,2,3,4])
+reciprocal=np.reciprocal(variable)
+print("The reciprocal is:",reciprocal)
