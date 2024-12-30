e58d607e8ab3f3f4b02cbe57b62f9ce2ffaca5fa
Umang Sharma <usharma1702@gmail.com>
2024-12-30 18:05:54 +0530

    Initial Commit

---
+++ Arithematic_operations_on_NumPy_Array.py
@@ -0,0 +1,13 @@
+# Performing Arithematic Operations On NumPy Arrays:-
+import numpy as np # Importing Numpy library
+variable1=np.array([1,2,3,4])
+variable2=np.array([5,6,7,8])
+print("The Sum of two array is:",variable1+variable2) # Addition
+print("\n")
+print("The Difference of two array is:",variable1-variable2) # Subtraction
+print("\n")
+print("The Divide of two array is:",variable1/variable2) # Divide
+print("\n")
+print("The Multiplication of two array is:",variable1*variable2) # Multiplication
+print("\n")
+print("The Modulus of two array is:",variable1%variable2) # Modulus
