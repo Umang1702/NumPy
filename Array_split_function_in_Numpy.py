55a0e9bb22925e385c9c08251bc951b3ccfe597e
Umang Sharma <usharma1702@gmail.com>
2024-12-31 13:44:50 +0530

    Initial Commit

--- Array_split_function_in_Numpy.py
+++ Array_split_function_in_Numpy.py
@@ -1,5 +1,12 @@
 import numpy as np  # Importing Numpy library.
-# split() function:-
+# split() function for 1D array:-
 variable= np.array([1,2,3,4,5,6])
 split= np.array_split(variable,3)# Use of split() function.
-print(split)
+print("Output of the split() function for 1D array:-\n",split)
+print("\n")
+
+
+# split() function for 2D array:-
+variable= np.array([[1,2],[3,4],[5,6]])
+split= np.array_split(variable,3)# Use of split() function.
+print("Output of the split() function for 2D array:-\n",split)
