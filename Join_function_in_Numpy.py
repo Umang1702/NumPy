acb0dd1c422f52be41ff7091384c94a984e31740
Umang Sharma <usharma1702@gmail.com>
2024-12-31 13:04:03 +0530

    Initial Commit

---
+++ Join_function_in_Numpy.py
@@ -0,0 +1,6 @@
+import numpy as np  # Improting Numpy Library.
+
+# concatenate() Function for 2D array:-
+var1=np.array([[1,2],[3,4]])
+var2=np.array([[1,2],[3,4]])
+print("Output of concatenate() function is:-",np.concatenate((var1,var2),axis=1)) # Use of concatenation() function.
