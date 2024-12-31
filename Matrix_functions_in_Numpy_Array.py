73d4e836204922750f8d15665bf3d146ea557fc3
Umang Sharma <usharma1702@gmail.com>
2024-12-31 15:07:19 +0530

    Initial Commit

--- Matrix_functions_in_Numpy_Array.py
+++ Matrix_functions_in_Numpy_Array.py
@@ -13,7 +13,7 @@ variable1 = np.matrix([[1,2,3],[5,6,7]])
 print("The Orignal array:-\n",variable1)
 print("\n")
 print("The output of swapaxes() function is:-\n",np.swapaxes(variable1,0,1))
-
+print("\n")
 
 
 # linalg.inv() function:-
@@ -21,7 +21,7 @@ variable2 = np.matrix([[1,2],[5,6]])
 print("The Orignal array:-\n",variable2)
 print("\n")
 print("The output of linalg.inv() function is:-\n",np.linalg.inv(variable2))
-
+print("\n")
 
 
 # linalg.matrix_power() function:-
@@ -29,7 +29,7 @@ variable3 = np.matrix([[1,2],[5,6]])
 print("The Orignal array:-\n",variable3)
 print("\n")
 print("The output of linalg.matrix_power() function is:-\n",np.linalg.matrix_power(variable3,0))
-
+print("\n")
 
 
 # linalg.det() function:-
